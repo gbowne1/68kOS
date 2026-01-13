180600d4fea5293c6d43fdea4e023b994c0cac00
Gregory Bowne <gbowne144@gmail.com>
2026-01-13 02:22:15 -0800

    Add task.h header file for task management

---
+++ kernel/sched/task.c
@@ -0,0 +1,25 @@
+/*
+ * kernel/sched/task.h
+ */
+
+#ifndef TASK_H
+#define TASK_H
+
+#include <stdint.h>
+
+typedef struct {
+    uint32_t regs[15]; /* d0-d7, a0-a6 */
+    uint32_t pc;       /* Program Counter */
+    uint32_t sr;       /* Status Register */
+    uint32_t usp;      /* User Stack Pointer */
+} cpu_context_t;
+
+typedef struct task {
+    uint32_t id;
+    cpu_context_t context;
+    struct task *next;
+} task_t;
+
+#endif
+
+/* End of file */
